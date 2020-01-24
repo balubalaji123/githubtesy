09fc7bf996e223e9bc06b0361375b3a47f18f904
unknown <balajipuvvada12289@gmail.com>
2020-01-24 18:24:59 +0530

    hi

--- src/app/tutor.ts
+++ src/app/tutor.ts
@@ -1,19 +1,14 @@
 export class Tutor {
     constructor(
-public img:string,        
-public subject:string,
-public time:string,
-public coursetype:string,
+public img: string,
+public subject: string,
+public time: string,
+public coursetype: string,
 public days: Object=[{"mon":false,"tue":false,"wed":false,"thu":false,"fri":false,"sat":false,"sun":false,"every":false}],
 // courseduration:number,
 public date: string,
-public fee:number,
-public description:string,
-public watsuplink:string
-
-
-
-
-
-    ){}
+public fee: number,
+public description: string,
+public watsuplink: string ,
+) {}
 }
